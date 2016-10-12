bcd860165f5892ce364e9e88a8ddd39b0d45dc83
Yuri Vuyan <vuyan@paragon-software.com>
2016-10-12 17:24:14 +0300

    Add nunit Framework and class Unit.cs

---
+++ Units.Tests/TransactionUnitTest.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Units;
+
+namespace Units.Tests
+{
+    [TestFixture]
+    public class TransactionUnitTest
+    {
+        [Test]
+        public void Test_Create_Unit()
+        {
+            var unitTest = new Unit();
+                
+            Assert.IsNotNull(unitTest);
+        }
+
+        [Test]
+        public void Test_GetOperationID()
+        {
+            var unitTest = new Unit();
+
+            var opID = unitTest.GetOperationId();
+
+          
+            Assert.IsNotNullOrEmpty(opID);
+        }
+
+    }
+}
---
+++ Units/Unit.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Interfaces;
+
+namespace Units
+{
+
+    public class Unit : ITransactionUnit
+    {
+       
+        public void Commit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetOperationId()
+        {
+            Guid g = Guid.NewGuid();
+
+
+            return g.ToString();
+        }
+
+        public void Rollback()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Rollback(string operationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+
+    }
+}
