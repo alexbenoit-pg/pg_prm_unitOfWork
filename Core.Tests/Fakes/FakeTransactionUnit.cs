8b97687910b4c35c0a514c6ccf3dad94e4dfb27b
Alexander Benoit <alexander.benoit@paragon-software.com>
2016-10-12 16:53:02 +0300

    Add methods to Core.Iterfaces.ITransactionUnit

--- Core.Tests/Fakes/FakeTransactionUnit.cs
+++ Core.Tests/Fakes/FakeTransactionUnit.cs
@@ -4,5 +4,25 @@
 
     class FakeTransactionUnit : ITransactionUnit
     {
+        public void Dispose()
+        {
+        }
+
+        public string GetOperationId()
+        {
+            return string.Empty;
+        }
+
+        public void Rollback(string operationID)
+        {
+        }
+
+        public void Rollback()
+        {
+        }
+
+        public void Commit()
+        {
+        }
     }
 }
--- Core/Interfaces/ITransactionUnit.cs
+++ Core/Interfaces/ITransactionUnit.cs
@@ -1,6 +1,29 @@
-﻿namespace Core.Interfaces
+﻿using System;
+
+namespace Core.Interfaces
 {
-    public interface ITransactionUnit
+    public interface ITransactionUnit:IDisposable
     {
+        /// <summary>
+        /// Return ID of this operation
+        /// </summary>
+        /// <returns>Unique ID</returns>
+        string GetOperationId();
+
+        /// <summary>
+        /// Rollback operation with <paramref name="operationId"/>
+        /// </summary>
+        /// <param name="operationId"></param>
+        void Rollback(string operationId);
+
+        /// <summary>
+        /// Rollback this operation
+        /// </summary>
+        void Rollback();
+
+        /// <summary>
+        /// Execute this operation
+        /// </summary>
+        void Commit();
     }
 }
