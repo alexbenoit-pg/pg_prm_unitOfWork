84afd1ba74c0f6bb9547f52a33def05ffaf982da
Yuri Vuyan <vuyan@paragon-software.com>
2016-10-17 15:26:41 +0300

    Add TransactionBadCommitUnit class, also add TransactionSimulationUnit with tests.

--- Units.Tests/TransactionUnitTest.cs
+++ Units.Tests/TransactionUnitTest.cs
@@ -1,34 +1,41 @@
-﻿using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Units;
-
-namespace Units.Tests
+﻿namespace Units.Tests
 {
+    using NUnit.Framework;
+    using System.IO;
+    using Assert = NUnit.Framework.Assert;
+
     [TestFixture]
     public class TransactionUnitTest
     {
         [Test]
-        public void Test_Create_Unit()
+        public void TransactionUnit_CreateInstance_IsNotNull()
         {
-            var unitTest = new Unit();
-                
+            var unitTest = new TransactionSimulationUnit();
+
             Assert.IsNotNull(unitTest);
         }
 
         [Test]
-        public void Test_GetOperationID()
+        public void TransactionUnit_GetOperationID_IsNotNull()
         {
-            var unitTest = new Unit();
+            var unitTest = new TransactionSimulationUnit();
+            var operationID = unitTest.GetOperationId();
 
-            var opID = unitTest.GetOperationId();
-
-          
-            Assert.IsNotNullOrEmpty(opID);
+            Assert.IsNotNullOrEmpty(operationID);
         }
+
+        [Test]
+        public void TransactionUnit_CheckCommite_IsTrue()
+        {
+            var unitTest = new TransactionSimulationUnit();
+            unitTest.Commit();
+            string testText = "Commit прошел успешно";
+            string[] text = File.ReadAllLines(unitTest.path);
+
+            Assert.IsTrue(text[text.Length-1] == testText);
+        }
+
+
 
     }
 }
